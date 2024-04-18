0eebc5daa7ca5842817e40c2a922b9df359b6052
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-19 00:51:32 +0200

    feat: add AppHeader Content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,13 @@ export default {
     data() {
         return {
             store,
+            isActive: true
         };
+    },
+    methods:{
+        showSearchBar() {
+            this.isActive = !this.isActive;
+        }
     }
 }
 </script>
@@ -14,15 +20,37 @@ export default {
 <template>
     <header>
         <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand" href="#">Boolflix</a>
+            <div class="container-fluid">
+                <a class="navbar-brand fs-3" href="#">Boolflix</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Serie TV</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Film</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nuovi e popolari</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">La mia lista</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sfoglia per lingua</a>
+                        </li>
+                    </ul>
                     <div class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" v-model="store.searchFilter">
-                        <button class="btn btn-success" @click="$emit('search')">Search</button>
+                        <div class="search-bar-container d-flex">
+                            <i class="bi bi-search text-light fs-4 ms-2" @click="$emit('search')"></i>
+                            <input class="search-bar form-control me-2" type="search" placeholder="Titoli, persone, generi" v-model="store.searchFilter">
+                        </div>
                     </div>
                 </div>
             </div>
@@ -30,4 +58,51 @@ export default {
     </header>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+header {
+    background-color: $brand-primary;
+
+    .navbar {
+        .navbar-brand {
+            color: $brand-secondary;
+            font-weight: 700;
+            text-transform: uppercase;
+        }
+    }
+
+    .navbar-nav {
+        .nav-link {
+            color: $brand-light;
+            &.active {
+                color: $brand-light;
+            }
+            &:hover {
+                color: $brand-secondary;
+            }
+        }
+    }
+
+    .search-bar-container {
+        border: 1px solid $brand-light;
+
+        .bi-search {
+            cursor: pointer;
+        }
+
+        .search-bar {
+            color: $brand-light;
+            border: 0;
+            background-color: transparent;
+            &:focus {
+                color: $brand-light;
+                box-shadow: none;
+            }
+            &::placeholder {
+                color: #c0bebe;
+            }
+        }
+    }
+}
+</style>

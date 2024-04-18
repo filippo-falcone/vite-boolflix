b2e0068effd9908b146d127e32affe2e1e53a836
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-18 10:16:05 +0200

    fix: AppResults

--- src/components/AppResults.vue
+++ src/components/AppResults.vue
@@ -14,10 +14,10 @@ export default {
 <template>
     <ul>
         <li v-for="movie in store.movies">
-            <h1>{{ movie.title }}</h1>
-            <h3>{{ movie.original_title }}</h3>
-            <div>{{ movie.original_language }}</div>
-            <div>{{ movie.vote_average }}</div>
+            <h1>Titolo: {{ movie.title }}</h1>
+            <h3>Titolo originale: {{ movie.original_title }}</h3>
+            <div>Lingua: {{ movie.original_language }}</div>
+            <div>Voto Medio: {{ movie.vote_average }}</div>
         </li>
     </ul>
 </template>

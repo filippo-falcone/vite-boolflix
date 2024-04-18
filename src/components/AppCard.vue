61e798696047c1f379496afce0e5cd5921c77628
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-18 13:50:08 +0200

    feat: add AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,52 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'AppCard',
+    props: {
+        cardInfo: Object
+    },
+    data() {
+        return {
+            flags: [
+                    {lang: 'it'},
+                    {lang: 'en'},
+                    {lang: 'fr'},
+                    {lang: 'de'},
+                    {lang: 'es'},
+                    {lang: 'ja'},
+                    {lang: 'cn'},
+                    {lang: 'ko'}
+                ],
+        };
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`../assets/img/${name}-flag.png`, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+    <li>
+        <h1>Titolo: {{ cardInfo.title || cardInfo.name}}</h1>
+        <h3>Titolo originale: {{ cardInfo.original_title || cardInfo.original_name }}</h3>
+        <div v-if="cardInfo.original_language === flags[0].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[0].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[1].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[1].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[2].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[2].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[3].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[3].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[4].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[4].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[5].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[5].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[6].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[6].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else-if="cardInfo.original_language === flags[7].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[7].lang)" :alt="cardInfo.original_language"></div></div>
+        <div v-else>Lingua: {{ cardInfo.original_language }}</div>
+        <div>Voto Medio: {{ cardInfo.vote_average }}</div>
+    </li>
+</template>
+
+<style scoped lang="scss">
+.flag-container {
+    max-width: 21px;
+}
+</style>
--- src/components/AppResults.vue
+++ src/components/AppResults.vue
@@ -1,28 +1,16 @@
 <script>
 import { store } from '../store';
+import AppCard from './AppCard.vue';
 
 export default {
     name: 'AppResults',
+    components: {
+        AppCard
+    },
     data() {
         return {
             store,
-            flags: [
-                {lang: 'it'},
-                {lang: 'en'},
-                {lang: 'fr'},
-                {lang: 'de'},
-                {lang: 'es'},
-                {lang: 'ja'},
-                {lang: 'cn'},
-                {lang: 'ko'}
-            ],
-            thisLang : null
         };
-    },
-    methods: {
-        getImageUrl(name) {
-            return new URL(`../assets/img/${name}-flag.png`, import.meta.url).href
-        }
     }
 }
 </script>
@@ -30,43 +18,16 @@ export default {
 <template>
     <div class="d-flex">
         <ul>
-            <li v-for="movie in store.movies">
-                <h1>Titolo: {{ movie.title }}</h1>
-                <h3>Titolo originale: {{ movie.original_title }}</h3>
-                <div v-if="movie.original_language === flags[0].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[0].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[1].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[1].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[2].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[2].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[3].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[3].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[4].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[4].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[5].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[5].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[6].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[6].lang)" :alt="movie.original_language"></div></div>
-                <div v-else-if="movie.original_language === flags[7].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[7].lang)" :alt="movie.original_language"></div></div>
-                <div v-else>Lingua: {{ movie.original_language }}</div>
-                <div>Voto Medio: {{ movie.vote_average }}</div>
-            </li>
+            films
+            <AppCard v-for="movie in store.movies" :key="movie.id" :cardInfo="movie"></AppCard>
         </ul>
         <ul>
-            <li v-for="tvSerie in store.tvSeries">
-                <h1>Titolo: {{ tvSerie.name }}</h1>
-                <h3>Titolo originale: {{ tvSerie.original_name }}</h3>
-                <div v-if="tvSerie.original_language === flags[0].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[0].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[1].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[1].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[2].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[2].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[3].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[3].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[4].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[4].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[5].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[5].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[6].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[6].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else-if="tvSerie.original_language === flags[7].lang" class="d-flex align-items-center">Lingua: <div class="flag-container ms-2"><img :src="getImageUrl(flags[7].lang)" :alt="tvSerie.original_language"></div></div>
-                <div v-else>Lingua: {{ tvSerie.original_language }}</div>
-                <div>Voto Medio: {{ tvSerie.vote_average }}</div>
-            </li>
+            series
+            <AppCard v-for="tvSerie in store.tvSeries" :key="tvSerie.id" :cardInfo="tvSerie"></AppCard>
         </ul>
     </div>
 
 </template>
 
 <style scoped lang="scss">
-.flag-container {
-    max-width: 21px;
-}
 </style>

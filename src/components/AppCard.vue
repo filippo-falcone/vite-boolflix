566369f06f20cbc7d348414e908249e640436d4e
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-19 11:40:22 +0200

    feat: add graphics to AppResults & AppCard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -36,38 +36,74 @@ export default {
 </script>
 
 <template>
-    <li>
-        <div v-if="cardInfo.poster_path === null" class="img-container"><img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="Image not found"></div>
-        <div v-else class="img-container"><img :src="'https://image.tmdb.org/t/p/w342' + cardInfo.poster_path" :alt="cardInfo.title || cardInfo.name"></div>
-        <h1>Titolo: {{ cardInfo.title || cardInfo.name}}</h1>
-        <h3>Titolo originale: {{ cardInfo.original_title || cardInfo.original_name }}</h3>
-        <div class="d-flex align-items-center">Lingua: 
-            <div class="flag-container ms-2">
-                <img v-if="flags.includes(cardInfo.original_language)" :src="getFlagUrl()" :alt="cardInfo.original_language">
-                <div v-else>{{ cardInfo.original_language }}</div>
+    <div class="col">
+        <div v-if="cardInfo.poster_path === null" class="img-container">
+            <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="Image not found">
+        </div>
+        <div v-else class="img-container">
+            <img :src="'https://image.tmdb.org/t/p/w342' + cardInfo.poster_path" :alt="cardInfo.title || cardInfo.name">
+        </div>
+        <div class="info-container p-3">
+            <div class="title">
+                Titolo:
+                <span>{{ cardInfo.title || cardInfo.name}}</span>
+            </div>
+            <div class="original title">
+                Titolo originale:
+                <span>{{ cardInfo.original_title || cardInfo.original_name }}</span>
+            </div>
+            <div class="d-flex align-items-center">
+                Lingua: 
+                <div class="flag-container ms-2">
+                    <img v-if="flags.includes(cardInfo.original_language)" :src="getFlagUrl()" :alt="cardInfo.original_language">
+                    <div v-else>{{ cardInfo.original_language }}</div>
+                </div>
+            </div>
+            <div class="d-flex">
+                Voto:
+                <!-- Prima soluzione -->
+                <!-- <div class="d-flex align-items-center ms-2">
+                    <i v-for="star in starArray" :key="star" :class="star" class="bi fs-6 text-warning"></i>
+                </div> -->
+                <!-- Seconda soluzione -->
+                <div class="d-flex align-items-center ms-2">
+                    <i v-for="i in 5" :class="{ 'bi-star-fill': i <= Math.round(cardInfo.vote_average / 2), 'bi-star': i > Math.round(cardInfo.vote_average / 2) }" class="bi fs-6 text-warning"></i>
+                </div>
             </div>
         </div>
-        <div class="d-flex">
-            Voto Medio:
-            <!-- Prima soluzione -->
-            <!-- <div class="d-flex align-items-center ms-2">
-                <i v-for="star in starArray" :key="star" :class="star" class="bi fs-6 text-warning"></i>
-            </div> -->
-            <!-- Seconda soluzione -->
-            <div class="d-flex align-items-center ms-2">
-                <i v-for="i in 5" :class="{ 'bi-star-fill': i <= Math.round(cardInfo.vote_average / 2), 'bi-star': i > Math.round(cardInfo.vote_average / 2) }" class="bi fs-6 text-warning"></i>
-            </div>
-        </div>
-    </li>
+    </div>
 </template>
 
 <style scoped lang="scss">
+.col {  
+    position: relative;
+    
+    .img-container {
+        max-width: 200px;
+    }
+    
+    .info-container {
+        font-size: .875rem;
+        background-color: #000000af;
+        position: absolute;
+        top: 0;
+        right: 4.5%;
+        bottom: 0;
+        left: 3.5%;
+        opacity: 0;
+        &:hover {
+            opacity: 1;
+        }
 
-.img-container {
-    width: 200px;
+        .title {
+            span {
+                color: #999797;
+            }
+        }
+        .flag-container {
+            max-width: 21px;
+        }
+    }
 }
-
-.flag-container {
-    max-width: 21px;
-}
+    
 </style>
--- src/components/AppResults.vue
+++ src/components/AppResults.vue
@@ -17,14 +17,16 @@ export default {
 
 <template>
     <div class="d-flex">
-        <ul>
-            films
-            <AppCard v-for="movie in store.movies" :key="movie.id" :cardInfo="movie"></AppCard>
-        </ul>
-        <ul>
-            series
-            <AppCard v-for="tvSerie in store.tvSeries" :key="tvSerie.id" :cardInfo="tvSerie"></AppCard>
-        </ul>
+        <div class="container">
+            <h2>Film</h2>
+            <div class="row row-cols-2 row-cols-sm-3 row-cols-lg-4 row-cols-xl-6 g-3">
+                <AppCard v-for="movie in store.movies" :key="movie.id" :cardInfo="movie"></AppCard>
+            </div>
+            <h2>Serie TV</h2>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-3">
+                <AppCard v-for="tvSerie in store.tvSeries" :key="tvSerie.id" :cardInfo="tvSerie"></AppCard>
+            </div>
+        </div>
     </div>
 
 </template>

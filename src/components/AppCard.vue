1ef1bb4ca8b6d9779b076a26b74977c932e0836f
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-19 12:08:59 +0200

    fix: AppCard layout

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -37,37 +37,35 @@ export default {
 
 <template>
     <div class="col">
-        <div v-if="cardInfo.poster_path === null" class="img-container">
-            <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="Image not found">
-        </div>
-        <div v-else class="img-container">
-            <img :src="'https://image.tmdb.org/t/p/w342' + cardInfo.poster_path" :alt="cardInfo.title || cardInfo.name">
-        </div>
-        <div class="info-container p-3">
-            <div class="title">
-                Titolo:
-                <span>{{ cardInfo.title || cardInfo.name}}</span>
-            </div>
-            <div class="original title">
-                Titolo originale:
-                <span>{{ cardInfo.original_title || cardInfo.original_name }}</span>
-            </div>
-            <div class="d-flex align-items-center">
-                Lingua: 
-                <div class="flag-container ms-2">
-                    <img v-if="flags.includes(cardInfo.original_language)" :src="getFlagUrl()" :alt="cardInfo.original_language">
-                    <div v-else>{{ cardInfo.original_language }}</div>
+        <div class="img-container">
+            <img v-if="cardInfo.poster_path === null" src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="Image not found">
+            <img v-else :src="'https://image.tmdb.org/t/p/w342' + cardInfo.poster_path" :alt="cardInfo.title || cardInfo.name">
+            <div class="info-container p-3">
+                <div class="title">
+                    Titolo:
+                    <span>{{ cardInfo.title || cardInfo.name}}</span>
                 </div>
-            </div>
-            <div class="d-flex">
-                Voto:
-                <!-- Prima soluzione -->
-                <!-- <div class="d-flex align-items-center ms-2">
-                    <i v-for="star in starArray" :key="star" :class="star" class="bi fs-6 text-warning"></i>
-                </div> -->
-                <!-- Seconda soluzione -->
-                <div class="d-flex align-items-center ms-2">
-                    <i v-for="i in 5" :class="{ 'bi-star-fill': i <= Math.round(cardInfo.vote_average / 2), 'bi-star': i > Math.round(cardInfo.vote_average / 2) }" class="bi fs-6 text-warning"></i>
+                <div class="original title">
+                    Titolo originale:
+                    <span>{{ cardInfo.original_title || cardInfo.original_name }}</span>
+                </div>
+                <div class="d-flex align-items-center">
+                    Lingua: 
+                    <div class="flag-container ms-2">
+                        <img v-if="flags.includes(cardInfo.original_language)" :src="getFlagUrl()" :alt="cardInfo.original_language">
+                        <div v-else>{{ cardInfo.original_language }}</div>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    Voto:
+                    <!-- Prima soluzione -->
+                    <!-- <div class="d-flex align-items-center ms-2">
+                        <i v-for="star in starArray" :key="star" :class="star" class="bi fs-6 text-warning"></i>
+                    </div> -->
+                    <!-- Seconda soluzione -->
+                    <div class="d-flex align-items-center ms-2">
+                        <i v-for="i in 5" :class="{ 'bi-star-fill': i <= Math.round(cardInfo.vote_average / 2), 'bi-star': i > Math.round(cardInfo.vote_average / 2) }" class="bi fs-6 text-warning"></i>
+                    </div>
                 </div>
             </div>
         </div>
@@ -75,10 +73,9 @@ export default {
 </template>
 
 <style scoped lang="scss">
-.col {  
-    position: relative;
-    
+.col {
     .img-container {
+        position: relative;
         max-width: 200px;
     }
     
@@ -87,9 +84,9 @@ export default {
         background-color: #000000af;
         position: absolute;
         top: 0;
-        right: 4.3%;
+        right: 0;
         bottom: 0;
-        left: 3.5%;
+        left: 0;
         opacity: 0;
         &:hover {
             opacity: 1;

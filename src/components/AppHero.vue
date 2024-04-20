9d34952e53cf4264229e4d8f80429868f439750e
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-20 16:49:46 +0200

    feat: add video over

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -11,11 +11,17 @@ export default {
                 <source src="../assets/videos/guardiani-della-galassia-vol-3.mp4" type="video/mp4">
             </video>
         </div>
+        <div class="video-over d-flex flex-column justify-content-end px-5">
+            <div class="img-container">
+                <img src="https://lumiere-a.akamaihd.net/v1/images/logo_gotg3_2_8cd40975.png" alt="Guardiani della Galassia vol 3 logo">
+            </div>
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
 .hero {
+    position: relative;
 
     .video-container {
         height: calc(100vh - 9.375rem);
@@ -24,6 +30,16 @@ export default {
             width: 100%;
             height: 100%;
             object-fit: cover;
+        }
+    }
+
+    .video-over {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+
+        .img-container {
+            width: 400px;
         }
     }
 }

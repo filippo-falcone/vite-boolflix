74152167b379edae2d000e5f316f119d06be049a
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-21 14:02:11 +0200

    feat: add title to videos array

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -5,34 +5,42 @@ export default {
             return {
                 videos: [
                     {
+                        title: 'One Piece',
                         video: 'YFbno_aPm0w',
                         img: 'occ-0-784-778.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABRyu-srP5sziC-IKbrb-wyzR-e0hrdgiJ8jTlH2hQiDxKsi6Kf6ZeZX52C2cgLiayA9CKe0WOKRuj5_gQ5EwEcBXY2ac8UKXRJbzP23fBNE.png?r=4a6'
                     },
                     {
+                        title: 'Spider-Man: Across The Spider-Verse',
                         video: 'x0y3vKmHC9s',
                         img: 'image.tmdb.org/t/p/w500/4JDOCXMbFv7blqAuDyUMzJEa8d.png'
                     },
                     {
+                        title: 'Disincanto',
                         video: '4Bug_PqnM90',
                         img: 'occ-0-784-778.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABai16sIKXucp1Bmkc5B7bW4uBiCXoGuZajooj5K-EuZyUiJlPhvaVc0kYaoumIyY_x61KsPd_39abi-uK1w2-XyT5bBPrTMbOw0C7uyLmiI0dXrb04TTxrzYro6Y8DL6TUOWMPyPuTjSL4NxOopzj1nz9eX11vPlkIBxu3Csyt-vrAgIZ2Fu.png?r=d1d'
                     },
                     {
+                        title: 'Porco Rosso',
                         video: 'jnxq_skL-wk',
                         img: 'occ-0-784-778.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABa2RFbqSUtoIe6SbGhmyRuDWKfgXkYLMW9XF-VKEgSz2L0y9UkkCN-LSJnFqkcz_vpNczDlPOJE5561sArqBq-x6187JDhgBRQ9ed9kVn6M.png?r=25c'
                     },
                     {
+                        title: 'La Leggenda di Korra',
                         video: 'qdLtZRG8Pz4',
                         img: 'occ-0-784-778.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABSsfAVAa1w8alEKxbj2P-CJSzN8l-xG5Yc2FxK1utwsklBjutTz7u97cvfD_x_hJkeEqNaH3K5u8fES0tqk5rhRDONxCNbQF9JcQNz3WrkU.png?r=6da'
                     },
                     {
+                        title: 'Guardiani della Galassia Volume 3',
                         video: 'NXhrB7uZCV4',
                         img: 'disney.images.edge.bamgrid.com/ripcut-delivery/v1/variant/disney/19913593-815d-4e9c-80b8-2d0ba9391ec7/trim?format=webp&max=800%7C300'
                     },
                     {
+                        title: 'Futurama',
                         video: 'Gv-1JGL58mI',
                         img: 'disney.images.edge.bamgrid.com/ripcut-delivery/v1/variant/disney/782c2cde-e7b4-44a3-8e1f-4e858345927e/trim?format=webp&max=800%7C300'
                     },
                     {
+                        title: 'Gli Incredibili 2',
                         video: '1N-a6ZJqw94',
                         img: 'disney.images.edge.bamgrid.com/ripcut-delivery/v1/variant/disney/5287f754-9806-4eae-aa6a-59b15d47d09f/trim?format=webp&max=800%7C300'
                     }
@@ -59,11 +67,11 @@ export default {
 <template>
     <section class="hero">
         <div class="video-container d-none d-lg-block">
-            <iframe id="player" :src="`https://www.youtube.com/embed/${videos[activeItem].video}?autoplay=1&mute=1&disablekb&controls=0&rel=0`" frameborder="0"></iframe>
+            <iframe :src="`https://www.youtube.com/embed/${videos[activeItem].video}?autoplay=1&mute=1&disablekb&controls=0&rel=0`"></iframe>
         </div>
         <div class="video-over d-flex flex-column justify-content-end px-5">
             <div class="img-container">
-                <img :src="`https://${videos[activeItem].img}`" alt="Guardiani della Galassia vol 3 logo">
+                <img :src="`https://${videos[activeItem].img}`" :alt="videos[activeItem].title">
             </div>
         </div>
     </section>
@@ -74,7 +82,7 @@ export default {
     position: relative;
 
     .video-container {
-        height: 550px;
+        height: 34.375rem;
 
         iframe {
             width: 100%;

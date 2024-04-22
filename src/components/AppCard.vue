dc12b1622b7d874b1fb8759f530f16df30facb84
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-22 09:52:21 +0200

    fix: added height 100% to posters in AppCard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -38,11 +38,14 @@ export default {
 <template>
     <div class="col">
         <div class="img-container">
-            <div v-if="cardInfo.poster_path === null" class="poster-not-found-container">
+            <div v-if="cardInfo.poster_path === null" class="poster-container not-found">
                 <img src="../assets/img/poster-place-holder.png" alt="poster-placeholder-background">
-                <img class="poster-not-found" src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="Image not found">
+                <img class="poster not-found" src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg" alt="Image not found">
             </div>
-            <img v-else :src="'https://image.tmdb.org/t/p/w342' + cardInfo.poster_path" :alt="cardInfo.title || cardInfo.name">
+            <div v-else class="poster-container">
+                <img src="../assets/img/poster-place-holder.png" alt="poster-placeholder-background">
+                <img class="poster" :src="'https://image.tmdb.org/t/p/w342' + cardInfo.poster_path" :alt="cardInfo.title || cardInfo.name">
+            </div>
             <div class="info-container p-3">
                 <div class="title">
                     Titolo:
@@ -79,19 +82,24 @@ export default {
 .col {
     .img-container {
         position: relative;
-        max-width: 200px;
 
-        .poster-not-found-container {
+        .poster-container {
             position: relative;
 
-            .poster-not-found {
-                width: 70%;
+            .poster {
+                height: 100%;
                 position: absolute;
-                top: 50%;
+                top: 0;
                 right: 0;
                 bottom: 0;
-                left: 50%;
-                transform: translate(-50%, -50%);
+                left: 0;
+                
+                &.not-found {
+                    width: 70%;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                }
             }
         }
     }

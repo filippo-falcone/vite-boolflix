62c6bcccfa0ec27f644dff6213e0f059104dce25
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-28 18:42:08 +0200

    fix: deleted window to app.vue

--- src/App.vue
+++ src/App.vue
@@ -83,7 +83,7 @@ export default {
     },
     mounted() {
         this.getTrendingMediaFromApi();
-        window.onscroll = () => {
+        onscroll = () => {
             this.showButtonToScroll();
         }
     }

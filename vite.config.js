8d002fb6b1c3e346f55a7f59b372a47cc7f47d8b
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-01 14:33:17 +0200

    feat: added GithubPages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+https://filippo-falcone.github.io/vite-boolflix/ https://github.com/filippo-falcone/vite-boolflix/ base '/vite-boolflix/'
 export default defineConfig({
   plugins: [vue()],
 })
